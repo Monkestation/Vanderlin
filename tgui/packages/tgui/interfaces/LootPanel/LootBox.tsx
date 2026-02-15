1a2db4c3a7fc4c3991b6e3ded529e80c13b36ebc
CheffieGithub <113442598+CheffieGithub@users.noreply.github.com>
2026-02-15 18:27:43 +0000

    that's button native

--- tgui/packages/tgui/interfaces/LootPanel/LootBox.tsx
+++ tgui/packages/tgui/interfaces/LootPanel/LootBox.tsx
@@ -38,14 +38,14 @@ export function LootBox(props: Props) {
       p={0}
       fluid
       color="transparent"
-      onClick={(event) => {
+      onClick={(event) =>
         act('grab', {
           alt: event.altKey,
           ctrl: event.ctrlKey,
           ref: item.ref,
           shift: event.shiftKey,
-        });
-      }}
+        })
+      }
       onAuxClick={(event) => {
         event.preventDefault();
         act('grab', {
